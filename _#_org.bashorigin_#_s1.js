c95db0a528bce967db0df9fcfe550aa2ff90f21a
Christoph Dorn <christoph@christophdorn.com>
2017-05-22 22:38:36 -0700

    more features

--- _#_org.bashorigin_#_s1.js
+++ _#_org.bashorigin_#_s1.js
@@ -5,7 +5,24 @@ const FS = require("fs");
 var templateCode = FS.readFileSync(PATH.join(__dirname, "minimal/index.html"), "utf8");
 
 
-exports.wrapHTML = function (code) {
-    code = templateCode.replace(/(<\/body>)/, code + "\n$1");
+exports.wrapHTML = function (bodyCode, options) {
+    options = options || {};
+
+    var code = templateCode;
+
+    if (options.uriDepth) {
+        var baseUri = [];
+        for (var i=0; i<options.uriDepth; i++) {
+            baseUri.push("..");
+        }
+        code = code.replace(/(href="(?!\/))/g, "$1" + baseUri.join("/") + "/");
+    }
+
+    if (options.css) {
+        code = code.replace(/(<\/head>)/, "<style>\n" + options.css + "\n</style>\n$1");
+    }
+
+    code = code.replace(/(<\/body>)/, bodyCode + "\n$1");
+
     return code;
 }
